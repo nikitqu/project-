720158bb2ef08c63d47db7f37f15b413c222057e
Nikita <example@mail.com>
2022-06-16 13:40:23 +0300

    Cycle & 2 part exersises

--- script.js
+++ script.js
@@ -60,7 +60,132 @@
 // console.log(obj.Jenny);
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//------------------------- 19. Практика, ч.1. Начинаем создавать приложение
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personMovieDB = {
+//     count: numberOfFilms,
+//     movie: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его', ''),   
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его', '');
+
+//       personMovieDB.movie[a] = b;
+//       personMovieDB.movie[c] = d;
+
+//      console.log(personMovieDB); 
+
+//  ----------------------------------------------УСЛОВИЯ 
+
+// if (4 == 9 ){
+//     console.log('OK');
+// }else {
+//     console.log('Error');
+// }
+
+// const num = 160;
+
+// if(num < 49 ){
+//     console.log('Мало');
+// } else if(num > 100){
+//     console.log('Много');
+// } else{
+//     console.log('OK!');
+// }
+
+// const hamburger = 5;
+// const fries = 4;
+
+// if (hamburger && fries) {
+//     console.log('Я Сыт!');
+// } 
+
+// console.log(hamburger && fries);  // true or false
+// console.log( NaN || 2 || undefined );  // 2
+// console.log( NaN && 2 && undefined ); // NaN
+// console.log( 1 && 2 && 3 ); // 3 
+// console.log( !1 && 2 || !3 ); // false 
+// console.log( 25 || null && !3 ); // 25
+// console.log( NaN || null || !3 || undefined || 5); // 5
+// console.log( NaN || null && !3 && undefined || 5); // 5
+// console.log( 5 === 5 && 3 > 1 || 5); // true
+
+//  -----------------------------------Циклы-----------------------------------
+
+
+// for (начало; условие; шаг) {
+//     // ... тело цикла ...
+//   }
+
+// let num = 50;
+
+// for (let i = 1; i < 8; i++){
+// console.log(num);
+// num++;
+// }
+
+//-------------------------------- (д) Цикл в цикле и метки
+
+
+// for (let i = 0; i < 3; i++){
+//     console.log(i);
+//     for (let j = 0; j < 3; j++){
+//         console.log(j);
+//     }
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let result = '';
+// let lenght = 7;
+
+// for(let i = 1; i < lenght; i++){
+//     for(let j = 0; j < i; j++){
+//         result += '*';
+//     }
+    
+//     result += '\n'; //перенос на след. строку
+// }
+
+// console.log(result);
+
+
+// // for(let i = 5; i <11; i++){
+// //     console.log(i);
+// // }
+
+
+// for (let i = 20; i >= 10; i--) {
+//     if (i === 13) break;
+//     console.log(i)
+// }
+
+// for (let i = 2; i <11; i++){
+
+//     console.log(i);
+// } 
+
+
+// const arr = [];
+//     for(let i =5; i<11;i++){
+//         arr[0] = i;
+//     }
+
+// -------------------------------24. Практика, ч2. Применяем условия и циклы
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personMovieDB = {
     count: numberOfFilms,
@@ -70,15 +195,44 @@ const personMovieDB = {
     private: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его', ''),   
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его', '');
 
-      personMovieDB.movie[a] = b;
-      personMovieDB.movie[c] = d;
+for(let i = 0; i<2; i++){
+
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его', '');
+
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personMovieDB.movie[a] = b;
+        console.log('Done!');
+    } else {
+        console.log('Error');
+        i--;
+
+    }
+
+}
+
+if (personMovieDB.count <10){
+    console.log('Просмотрено мало фильмов');
+} else if (personMovieDB.count >= 10 && personMovieDB.count < 30){
+    console.log('Вы классический зритель');
+}else if(personMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else{
+    console.log('Произошла ошибка');
+}
 
      console.log(personMovieDB); 
+
+
+
+
+
+
+
+
+
+
 
 
 
